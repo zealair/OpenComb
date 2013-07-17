f6261e8b09d226647573756d1fd44f92bde7cd67
aleechou <zq@alee2002.com>
2013-07-17 10:38:39 +0800

    fix: keywords "test", "node_moduels" in .gitignore

--- test.js
+++
@@ -1,9 +0,0 @@
-var m = require("module") ;
-
-var ful = require.resolve("./test-b.js") ;
-var b = require("./test-b.js") ;
-delete require.cache[ful] ;
-var b = require("./test-b.js") ;
-delete require.cache[ful] ;
-
-var b = require("./test-b.js") ;
